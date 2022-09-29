722c2ac0be768724a79e1907ffa7feb90e9c5353
Marcus Johnson <mjthunder@Marcuss-Air.localdomain>
2022-09-28 18:09:38 -0600

    Problem 9 and 10 are updated

--- lab.js
+++ lab.js
@@ -105,7 +105,10 @@ let passingScore = 7;
 */
 
 //CODE HERE
-
+while  (score < passingScore) {
+  console.log("Your score is not high enough");
+  score = score + 1;
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
@@ -116,12 +119,30 @@ let passingScore = 7;
 */
 
 //CODE HERE
+// let changeMyMind = true;
+// while (changeMyMind === true) {
+//   changeMyMind = false;
+//   console.log(changeMyMind);
+//   if (changeMyMind === false) {
+//     changeMyMind = true;
+//     console.log(changeMyMind)
+//   }
+// }
+let changeMyMind = true;
+if (changeMyMind === true) {
+  changeMyMind = false;
+  console.log(changeMyMind)
+} else {
+  changeMyMind = true;
+}
+
 
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
-
+changeMyMind = true;
+console.log(changeMyMind);
 ////////// ADVANCED PROBLEMS //////////
 
 ////////// PROBLEM 11 //////////
